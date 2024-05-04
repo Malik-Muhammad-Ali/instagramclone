23dd4e9f671237ed127740721b6a4106bd4f69d5
Malik Muhammad Ali <muhammadali556656@gmail.com>
2024-05-05 00:07:46 +0500

    First Commit

---
+++ routes/users.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const plm = require("passport-local-mongoose");
+
+mongoose.connect("mongodb://127.0.0.1:27017/instagram");
+
+const userSchema = mongoose.Schema({
+  username: String,
+  name: String,
+  email: String,
+  password: String,
+  picture: {
+    type: String,
+    default: "def.png"
+  },
+  contact: String,
+  bio: String,
+  stories: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "story" 
+    }
+  ],
+  saved: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "post" 
+    }
+  ],
+  posts: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "post" 
+  }],
+  followers: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user" 
+    } 
+  ],
+  following: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user" 
+    }
+  ]
+})
+
+userSchema.plugin(plm);
+
+module.exports = mongoose.model("user", userSchema);
